d2dc9cdc436acfe9dc8b10167c8bae8446ee1e95
Z-rim <49464600+Z-rim@users.noreply.github.com>
2023-02-09 12:17:32 -0500

    Add files via upload

---
+++ App.java
@@ -0,0 +1,33 @@
+public class App {import java.util.Random;
+
+    public Sort name() {
+        public static int[] creatRandomArray(int arrayLength) {
+           Random random = new Random();
+           in[] array = new int[arrayleangth];
+           for () 
+            
+        }
+        return array;
+
+    }
+    
+    public static boolean isSorted(inrt[] array) {
+        for(int i = 0; i < array.length-1; i++){
+            if (array[i] > array[i+1]){
+                return false;
+            }   
+        }
+       return true;
+
+    }
+    import java.toFile;//
+    try {
+File myObj = new FiLe("fiLename. txt");
+if (myobj. createNewFile()) {
+system.out. println("File created. ");
+} else {
+System.out, println("File already exists.");
+  } catch (IOException e) {
+ System.out. print[n("An error occurred.");
+e,printStackTrace();
+}
